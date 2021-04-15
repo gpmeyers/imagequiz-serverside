89948037bbd60f6fb75c0b76a5f51e53028b2f46
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2021-04-14 22:32:33 -0700

    Added stuff for CORS.

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 // dependencies
 const express = require('express');
+const cors = require('cors');
 const data = require('./data');
 
 // create the server
 const app = express();
 const port = process.env.PORT || 4000;
 
+// use cors
+var corsOptions = {
+    origin: 'https://gpmeyers.github.io',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
+
+app.use(cors(corsOptions));
+
 // parse json
 app.use(express.json());
 
 // data
 let scores = [];
+
+app.use(cors(corsOptions));
 
 app.get('/quizzes', (req, res) => {
     res.json(data.quizzes);
